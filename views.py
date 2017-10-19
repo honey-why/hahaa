ba24316cfdc7c67c387bed14518ccf1778a25f41
honey-why <xwybnxmwrujqi@163.com>
2017-10-19 17:56:31 +0800

    chuangjian

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
 from django.http import HttpResponse
     def index(request):
-        return　HttpResponse
+        return　HttpResponse("hello")
+    def index2(request):
+        return HttpResponse('index2')
